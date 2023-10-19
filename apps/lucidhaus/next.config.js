0ffa12ff96cf21eff7109ecc9db6cfe5102ab0dc
taayyohh <milesxbridges@gmail.com>
2023-10-19 15:13:51 -0400

    update to use infura

--- apps/lucidhaus/next.config.js
+++ apps/lucidhaus/next.config.js
@@ -8,13 +8,11 @@ const nextConfig = {
     MERKLE: process.env.MERKLE,
   },
   images: {
-    domains: ['ipfs.io', 'nftstorage.link', 'ipfs.zora.co'],
+    domains: ['lucidhaus.infura-ipfs.io'],
   },
   webpack(config) {
     // Grab the existing rule that handles SVG imports
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.('.svg'),
-    )
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'))
 
     config.module.rules.push(
       // Reapply the existing rule, but only for svg imports ending in ?url
@@ -29,7 +27,7 @@ const nextConfig = {
         issuer: /\.[jt]sx?$/,
         resourceQuery: { not: /url/ }, // exclude if *.svg?url
         use: ['@svgr/webpack'],
-      },
+      }
     )
 
     // Modify the file loader rule to ignore *.svg, since we have it handled now.
