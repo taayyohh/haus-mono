51368d6d9635645f283382777549b06eb660c494
taayyohh <milesxbridges@gmail.com>
2023-10-26 21:34:51 -0400

    add try catch

--- apps/lucidhaus/src/app/api/stripe/onramp/route.ts
+++ apps/lucidhaus/src/app/api/stripe/onramp/route.ts
@@ -3,17 +3,25 @@ import { OnrampSessionResource, stripe } from '@/stripe/stripe-sdk'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(req: NextRequest) {
-  const { transaction_details } = await req.json()
+  try {
+    const { transaction_details } = await req.json()
 
-  const onrampSession = new OnrampSessionResource(stripe).create({
-    transaction_details: {
-      destination_currency: transaction_details['destination_currency'],
-      // destination_exchange_amount: transaction_details['destination_exchange_amount'],
-      destination_network: transaction_details['destination_network'],
-      source_amount:  transaction_details['source_amount']
-    },
-  })
-  const [session] = await Promise.all([onrampSession])
+    console.log('DET', transaction_details)
 
-  return NextResponse.json({ data: session }, { status: 200 })
+    const onrampSession = new OnrampSessionResource(stripe).create({
+      transaction_details: {
+        destination_currency: transaction_details['destination_currency'],
+        // destination_exchange_amount: transaction_details['destination_exchange_amount'],
+        destination_network: transaction_details['destination_network'],
+        source_amount: transaction_details['source_amount'],
+      },
+    })
+    const [session] = await Promise.all([onrampSession])
+
+    return NextResponse.json({ data: session }, { status: 200 })
+  } catch (e) {
+    console.log('err', e)
+
+    return NextResponse.json({ data: e }, { status: 500 })
+  }
 }
