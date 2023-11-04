acba461f0a8b97796bda056d98590c0facda6d23
taayyohh <milesxbridges@gmail.com>
2023-11-03 23:17:22 -0400

    use axios

--- apps/lucidhaus/src/modules/blog/utils/fetchBlogPostByArtist.ts
+++ apps/lucidhaus/src/modules/blog/utils/fetchBlogPostByArtist.ts
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import config from '@/constants/config'
 import { ObjectId } from 'mongoose'
 import { IBlogPost } from '@/models/Blog'
@@ -8,25 +9,22 @@ export async function fetchBlogsByArtist(
   limit: number = 10 // default limit
 ): Promise<{ data: IBlogPost[] }> {
   try {
-    const response = await fetch(`${config.BASE_URL}api/blog/artist`, {
-      // This endpoint should match your actual endpoint
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ primaryArtistId: artistId, page, limit }),
+    const response = await axios.post(`${config.BASE_URL}api/blog/artist`, {
+      primaryArtistId: artistId,
+      page,
+      limit,
     })
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`)
-    }
-
-    const data = await response.json()
-
     return {
-      data,
+      data: response.data,
     }
   } catch (err) {
+    if (axios.isAxiosError(err)) {
+      console.error(`Error: ${err.message}`)
+    } else {
+      // Handle unknown errors
+      console.error(`Error: ${err}`)
+    }
     return { data: [] as IBlogPost[] }
   }
 }
