cf0eaeaf5d0c7e5f322346d6e480e2e5c66122b8
taayyohh <milesxbridges@gmail.com>
2025-11-05 08:08:04 -0500

    update ipfs gateway

--- apps/lucidhaus/src/utils/getIpfsGetway.ts
+++ apps/lucidhaus/src/utils/getIpfsGetway.ts
@@ -1,3 +1,6 @@
 export const getIpfsGateway = (uri: string): string => {
-  return uri?.replace('ipfs://', 'https://lucidhaus.infura-ipfs.io/ipfs/')
+  if (!uri || !uri.startsWith('ipfs://')) {
+    return ''
+  }
+  return uri.replace('ipfs://', 'https://ponder-finance.mypinata.cloud/ipfs/') + '?pinataGatewayToken=0jL5Qyjh-XEoTm-JHEX3Ye_IXh8eNUJA5X5CO9G98PSLYiE_-3KaxZXjR5rySbIz'
 }
